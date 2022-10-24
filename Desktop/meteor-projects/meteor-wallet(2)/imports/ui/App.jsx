65a76805788416ed9feb73660a4e0374bc9bdc20
효승 <hyoseung@hyoseung-ui-MacBook-Pro.local>
2022-10-24 13:41:18 +0900

    UI

---
+++ Desktop/meteor-projects/meteor-wallet(2)/imports/ui/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Wallet from "./components/Wallet";
+import { ContactForm } from "./ContactForm";
+import { ContactList } from "./ContactList";
+import { Header } from "./Header";
+
+export const App = () => (
+  <div>
+    <Header />
+    <div className="min-h-full">
+      <div className="max-w-4xl mx-auto p-2">
+        <Wallet />
+        <ContactForm />
+        <ContactList />
+      </div>
+    </div>
+  </div>
+);
